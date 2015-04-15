b840dbbb3aa1ba0b5d8bf3cefc4b67bd1a3ea459
Ritchy <chitankensuru@gmail.com>
2015-04-15 18:11:04 +0200

    Ruby::Hamming::4

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,6 @@
 class Hamming
   def self.compute(dna_strand_a, dna_strand_b)
-    dna_zip = dna_strand_a.chars.zip(dna_strand_b.chars)
-    dna_zip.count do |a, b|
+    dna_strand_a.chars.zip(dna_strand_b.chars).count do |a, b|
        a != b
     end
   end
