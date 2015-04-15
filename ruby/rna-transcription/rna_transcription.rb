d190069f887515ff55fb6623246f52a00d509121
Ritchy <chitankensuru@gmail.com>
2015-04-15 23:00:00 +0200

    Ruby::Rna_transcription::2

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,14 +1,18 @@
 class Complement
+  DNA_TO_RNA = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
+  RNA_TO_DNA = DNA_TO_RNA.invert
 
   def self.of_dna(dna)
-    dict = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
-    raise(ArgumentError, "Incorrect DNA strands") unless dict.key?(dna)
-    dna.gsub(/[GCTA]/,dict)
+    complement(dna, DNA_TO_RNA)
   end
 
   def self.of_rna(rna)
-    dict = { 'C' => 'G', 'G' => 'C', 'A' => 'T', 'U' => 'A' }
-    raise(ArgumentError, "Incorrect RNA strands") unless dict.key?(rna)
-    rna.gsub(/[CGAU]/,dict)
+    complement(rna, RNA_TO_DNA)
   end
+
+  private
+    def self.complement(nucleotide, complement_hash)
+      raise(ArgumentError, "Incorrect RNA strands") unless complement_hash.key?(nucleotide)
+      nucleotide.gsub(/[#{complement_hash.keys.join}]/, complement_hash)
+    end
 end
