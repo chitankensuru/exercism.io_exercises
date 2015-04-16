89ca216c7065febdf76dd65732cdeafbc287b9ec
Ritchy <chitankensuru@gmail.com>
2015-04-16 14:35:36 +0200

    Javascript::Bob::2

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,21 +1,31 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//Iteration 2
+// Move out validations to their own functions
 
 var Bob = function() {};
 
-Bob.prototype.hey = function(input) {
-    if (input == null || input.trim() === '') {
+Bob.prototype.hey = function (input) {
+    if (this.isSilent(input)) {
         return 'Fine. Be that way!'
-    } else if (input === input.toUpperCase() &&  input.isNumber() == false) {
+    } else if (this.isShouting(input)) {
         return 'Whoa, chill out!';
-    } else if (input.charAt(input.length-1) === '?') {
+    } else if (this.isQuestion(input)) {
         return 'Sure.'
     } else {
         return 'Whatever.'
     }
 };
+
+Bob.prototype.isSilent = function (input){
+    return (input == null || input.trim() === '')
+}
+
+Bob.prototype.isShouting = function (input) {
+    return (input === input.toUpperCase() &&  input.isNumber() == false)
+}
+
+Bob.prototype.isQuestion = function (input) {
+    return (input.charAt(input.length-1) === '?')
+}
 
 String.prototype.isNumber = function() {
     return /^\d+$/.test(this);
